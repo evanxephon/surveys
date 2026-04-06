54dde8e330872aacf42a62070534e34b0213ead6
evanxephon <evanxephon@gmail.com>
2026-04-06 10:04:47 +0800

    Update site title and share links

--- src/config/share.ts
+++ src/config/share.ts
@@ -1,5 +1,5 @@
 export const SHARE_CONFIG = {
   shareSubtitle: '你是陀思妥耶夫斯基笔下的哪个角色？',
-  fallbackShareUrl: 'https://qualia.wiki',
-  xiaohongshuPlaceholderUrl: 'https://qualia.wiki',
+  fallbackShareUrl: 'https://dostoevsk.qualia.wiki',
+  xiaohongshuPlaceholderUrl: 'https://dostoevsk.qualia.wiki',
 };
